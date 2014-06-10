6233064fe65672a133b27661cf78e1713e4e518a
Tasso (Ich) <luckydonald@flutterb.at>
2014-06-10 20:10:40 +0200

    Initial Commit

---
+++ helloC/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  helloC
+//
+//  Created by Tasso (Ich) on 10.06.14.
+//  Copyright (c) 2014 Tasso (Ich). All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
